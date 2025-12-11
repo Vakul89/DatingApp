2f7df8c9f25c0907021cf0c9dfafcee467367534
Vakul89 <vakul_2007@yahoo.com>
2025-12-11 00:20:31 -0600

    Profile and Themes added

--- client/src/core/services/account-service.ts
+++ client/src/core/services/account-service.ts
@@ -1,7 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable, signal } from '@angular/core';
-import { Logincreds, RegisterCreds, User } from '../../types/user';
+import { Logincreds, User } from '../../types/user';
 import { tap } from 'rxjs';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
@@ -10,7 +11,7 @@ import { tap } from 'rxjs';
 export class AccountService {
   private http = inject(HttpClient);
   currentUser = signal<User | null>(null);
-  baseUrl = 'https://localhost:5001/api/';
+  private baseUrl = environment.apiUrl;
 
   login(creds: any) {
     return this.http.post<User>(this.baseUrl + 'account/login', creds).pipe(
--- client/src/layout/nav/nav.ts
+++ client/src/layout/nav/nav.ts
@@ -1,8 +1,9 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AccountService } from '../../core/services/account-service';
 import { Router, RouterLink, RouterLinkActive } from "@angular/router";
 import { ToastService } from '../../core/services/toast-service';
+import { themes } from '../theme';
 
 @Component({
   selector: 'app-nav',
@@ -11,15 +12,30 @@ import { ToastService } from '../../core/services/toast-service';
   styleUrl: './nav.css',
 })
 
-export class Nav {
+export class Nav implements OnInit {
   protected accountService = inject(AccountService);
   private router = inject(Router);
   private toast = inject(ToastService);
   protected creds: any = {}
+  protected selectedTheme = signal<string>(localStorage.getItem('theme') || 'light');
+  protected themes = themes;
+
+  ngOnInit(): void {
+    document.documentElement.setAttribute('data-theme', this.selectedTheme());
+  }
+
+  handleSelectedTheme(theme: string) {
+    this.selectedTheme.set(theme);
+    localStorage.setItem('theme', theme);
+    document.documentElement.setAttribute('data-theme', theme);
+
+    const elem = document.activeElement as HTMLDivElement;
+    if (elem) elem.blur();
+  }
 
   login() {
     this.accountService.login(this.creds).subscribe({
-      next: result => {
+      next: () => {
         this.router.navigateByUrl('/members');
         this.toast.success('Login successfully');
         this.creds = {};
