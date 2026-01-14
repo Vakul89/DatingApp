d40eefcb5a1e7d2bde8185c392718f8cf62a2526
Vakul89 <vakul_2007@yahoo.com>
2026-01-13 18:12:56 -0600

    UI Changes

--- API/Services/TokenService.cs
+++ API/Services/TokenService.cs
@@ -33,6 +33,7 @@ public class TokenService(IConfiguration config, UserManager<AppUser> userManage
         {
             Subject = new ClaimsIdentity(claims),
             Expires = DateTime.UtcNow.AddMinutes(7),
+            ////Expires = DateTime.UtcNow.AddDays(14),
             SigningCredentials = creds
         };
 
--- client/src/core/services/account-service.ts
+++ client/src/core/services/account-service.ts
@@ -74,7 +74,8 @@ export class AccountService {
           this.logout();
         }
       })
-    }, 5 * 60 * 1000)
+    }, 5 * 60 * 1000) // 5 minutes
+    ////}, 14 * 24 * 60 * 60 * 1000) // 14 days
   }
 
   private getRolesFromToken(user: User): string[] {
